7443611e6732bc1f1f99c7a72adcc6da1fbff195
Aurel Feer <aurel.feer@gmail.com>
2019-03-03 22:58:54 +0100

    new dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-extern crate glfw;
 extern crate noise;
 
 mod util;
 
-use glfw::{Action, Context, Key};
 //use noise::{NoiseFn, Perlin};
 
 //static TICK_RATE: i64 = 64; //updates of the game logic per second
@@ -11,17 +9,4 @@ use glfw::{Action, Context, Key};
 
 fn main() {
 
-	let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-
-	let (mut window, events) = glfw.create_window(1500, 950, "Hello this is window", glfw::WindowMode::Windowed)
-		.expect("Failed to create GLFW window.");
-
-	window.set_key_polling(true);
-	window.make_current();
-
-	while !window.should_close() {
-		glfw.poll_events();
-
-		window.swap_buffers();
-	}
 }
