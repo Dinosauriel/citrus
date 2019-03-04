4ee284d12b6521c218d2da53fa49a722cb267f89
Aurel Feer <aurel.feer@gmail.com>
2019-03-04 22:49:57 +0100

    new dependenies for the 1000th time

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 extern crate noise;
+extern crate gfx;
+extern crate winit;
 
-mod util;
+
+use winit::EventsLoop;
+use gfx::traits::FactoryExt;
+use gfx::Device;
+
+pub type ColorFormat = gfx::format::Srgba8;
+pub type DepthFormat = gfx::format::DepthStencil;
+
+const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
 //use noise::{NoiseFn, Perlin};
 
@@ -8,5 +18,12 @@ mod util;
 //static INGAME_TICK_RATE: i64 = 64; //units of ingame time per second. influences game speed
 
 fn main() {
+	let mut events_loop = winit::EventsLoop::new();
+	let window = winit::Window::new(&events_loop).unwrap();
 
+	let mut running = true;
+	while running {
+		
+
+	}
 }
