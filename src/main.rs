027a18001be242182862fffa97cad604bb566f17
Aurel Feer <aurel.feer@gmail.com>
2019-03-04 23:15:05 +0100

    replaced gfx by gfx_hal

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
+#[cfg(windows)]
+extern crate gfx_backend_dx12 as backend;
+#[cfg(target_os = "macos")]
+extern crate gfx_backend_metal as backend;
+#[cfg(all(unix, not(target_os = "macos")))]
+extern crate gfx_backend_vulkan as backend;
+
 extern crate noise;
-extern crate gfx;
+extern crate gfx_hal;
 extern crate winit;
 
 
-use winit::EventsLoop;
-use gfx::traits::FactoryExt;
-use gfx::Device;
-
-pub type ColorFormat = gfx::format::Srgba8;
-pub type DepthFormat = gfx::format::DepthStencil;
-
-const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+use winit::{Event, EventsLoop, KeyboardInput, VirtualKeyCode, WindowBuilder, WindowEvent};
+use gfx_hal::{
+    command::{ClearColor, ClearValue},
+    format::{Aspects, ChannelType, Format, Swizzle},
+    image::{Access, Layout, SubresourceRange, ViewKind},
+    pass::{
+        Attachment, AttachmentLoadOp, AttachmentOps, AttachmentStoreOp, Subpass, SubpassDependency,
+        SubpassDesc, SubpassRef,
+    },
+    pool::CommandPoolCreateFlags,
+    pso::{
+        BlendState, ColorBlendDesc, ColorMask, EntryPoint, GraphicsPipelineDesc, GraphicsShaderSet,
+        PipelineStage, Rasterizer, Rect, Viewport,
+    },
+    queue::Submission,
+    Backbuffer, Device, FrameSync, Graphics, Instance, Primitive, Surface, SwapImageIndex,
+    Swapchain, SwapchainConfig,
+};
 
 //use noise::{NoiseFn, Perlin};
 
@@ -18,12 +35,17 @@ const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 //static INGAME_TICK_RATE: i64 = 64; //units of ingame time per second. influences game speed
 
 fn main() {
-	let mut events_loop = winit::EventsLoop::new();
-	let window = winit::Window::new(&events_loop).unwrap();
+	let mut events_loop = EventsLoop::new();
 
-	let mut running = true;
-	while running {
-		
+	let window = WindowBuilder::new()
+		.with_title("Part 00: Triangle")
+		.with_dimensions((256, 256).into())
+		.build(&events_loop)
+		.unwrap();
 
-	}
+	let instance = backend::Instance::create("Part 00: Triangle", 1);
+
+	let mut surface = instance.create_surface(&window);
+
+	let mut adapter = instance.enumerate_adapters().remove(0);
 }
